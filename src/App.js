002eafc975191b595b5447105e8f6bec4faeb7e7
Geeeva <ivana.gilich@gmail.com>
2019-06-10 16:59:58 +0200

    adding bounderies for exiting

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div>
-    </div>
-  );
+const a = [4, 35, 80, 123, 12345, 44, 8, 5, 24, 3], k = 4;
+
+const arrayPrinting = (a, k) => {
+    //Setting bounderies
+    //N is an integer within the range [1..200] - length of array and if no exiting the function
+    if(a.length === 0 || a.length > 200) {
+        console.log("Array has no elements or more than 200 elements");
+        return;
+    }
+
+    //k is not an integer within the range [1..1,000,000,000] - break, or k is not an integer, or k is greater than the length of array exiting the function
+    if( k < 1 || k > 1000000 || !Number.isInteger(k) || k > a.length) {
+        console.log("Break - k is smaller than 1 or has value greater than 1000000, or k is not an integer, or k greater than the length of array, therefore array cannot be divided into rows");
+        return;
+    } 
+
+    //Element of array a is an integer within the range [0..1,000,000,000], if no exiting the function
+    for(let i = 0; i < a.length; i++){
+        if(a[i] < 0 || a[i] > 1000000  || !Number.isInteger(a[i])){
+            console.log("There are other types of elements other than integer within array, or are smaller than 1 or are greater than 1000000");
+            return;
+        }
+    }
+}
+const App = () => {
+    return (
+        <div>
+            
+        </div>
+    );
 }
 
 export default App;
