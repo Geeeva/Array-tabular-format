4840c37a9e24d4e7ea6827fab28e2617d828b210
Geeeva <ivana.gilich@gmail.com>
2019-07-16 22:53:05 +0200

    Updates

---
+++ precache-manifest.6836857a75ba877a45d87e99e007e35d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9483d671d0d25ac1ac117d438764b119",
+    "url": "/Array-tabular-format.git/index.html"
+  },
+  {
+    "revision": "f961a857d9d72eb61f9d",
+    "url": "/Array-tabular-format.git/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "dced4f20ede559ab4549",
+    "url": "/Array-tabular-format.git/static/js/2.ca7a3741.chunk.js"
+  },
+  {
+    "revision": "f961a857d9d72eb61f9d",
+    "url": "/Array-tabular-format.git/static/js/main.4c2af809.chunk.js"
+  },
+  {
+    "revision": "c1c7edeba3b9e75442be",
+    "url": "/Array-tabular-format.git/static/js/runtime~main.4ae010ef.js"
+  }
+]);
